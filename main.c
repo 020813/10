6831e73dbac7c4d8e764edbdadd494558b52e227
020813 <113406823+020813@users.noreply.github.com>
2022-11-08 17:12:58 +0900

    06

--- main.c
+++ main.c
@@ -6,23 +6,14 @@
 
 int main(int argc, char *argv[]) {
 	
-	FILE * fp;
-	char str[30];
+	FILE *fp = NULL;
+	char c;
 	
-	fp = fopen("sample.txt", "w");
-	
-	printf("input a word: ");
-	scanf("%s", str);
-	fprintf(fp, "%s\n", str);
-	
-	printf("input a word: ");
-	scanf("%s", str);
-	fprintf(fp, "%s\n", str);
-	
-	printf("input a word: ");
-	scanf("%s", str);
-	fprintf(fp, "%s\n", str);
-	
+	fp = fopen("sample.txt", "r");
+	 
+	while( (c = fgetc(fp)) != EOF)
+	    putchar(c);
+	 
 	fclose(fp);
 		
 	return 0;
